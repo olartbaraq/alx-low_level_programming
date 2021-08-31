47b0da0a5d358702e42f8a8840fff8e48e4cba63
Akanbi Mubaraq <akanbi.ges201@gmail.com>
2021-08-31 10:24:52 -0700

    program that prints Holberton, followed by a new line.
    The program should return 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - program that prints Holberton.
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+	int n = 0;
+	char str_hbt[9] = "Holberton";
+
+	while (n < 9)
+	{
+		_putchar(str_hbt[n]);
+		n += 1;
+	}
+	_putchar('\n');
+	return (0);
+}
