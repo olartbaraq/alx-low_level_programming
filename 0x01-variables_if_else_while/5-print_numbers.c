2672aff4f89f673c90c15622e0e37a1765b63662
olartbaraq <88320030+olartbaraq@users.noreply.github.com>
2021-09-12 00:45:29 +0100

    update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,14 +5,15 @@
  * All your code should be in the main function
  * Return: 0
  */
- 
+
 int main(void)
 {
-int i ;
-for(i=0 ; i<10 ; ++i) 
+int n;
+while (n < 10)
 {
-printf("%d", i);
+printf("%d", n);
+n += 1;
 }
-putchar('\n');
-return 0;    
+printf("\n");
+return (0);
 }
