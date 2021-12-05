ec80a6a20539476f48260ff956994cd3160a73aa
Olartbaraq <akanbi.ges201@gmail.com>
2021-12-05 16:46:24 +0100

    update the return null

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,9 +11,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new, *temp;
 new = malloc(sizeof(dlistint_t));
+if (!new)
+return NULL;
 new->n = n;
 new->prev = NULL;
-new->next = NULL;
+new->next = NULL;     
+if (!head)
+return (NULL);
 if (*head == NULL)
 *head = new;
 else
