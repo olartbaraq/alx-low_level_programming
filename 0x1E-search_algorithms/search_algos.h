d075a274083b0c93a4badac3649ecfe714891d25
olart <akanbi.ges201@gmail.com>
2022-03-29 20:49:27 +0100

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
-#ifndef __SEARCH_ALGOS_H
-#define __SEARCH_ALGOS_H
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -8,4 +11,4 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 void print_message(int *array, size_t size);
 
-#endif /* __SEARCH_ALGOS_H */
+#endif
